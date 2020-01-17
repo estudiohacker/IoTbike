0f818ef1236d724207e8fcbb4e7694eb644ed604
João Adriano Freitas <jaafreitas@gmail.com>
2020-01-16 23:50:55 -0300

    Nova tela de abertura e mensagens de iniciação. Ajustes de posicionamento nas demais telas.

--- IoTbike.ino
+++ IoTbike.ino
@@ -31,15 +31,6 @@ double Home_LNG = 0.0;
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ D6, /* data=*/ D5, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
 
-//sat20x20px logo
-static const unsigned char u8g_logo_sat[] U8X8_PROGMEM = {
-  0x00, 0x01, 0x00, 0x80, 0x07, 0x00, 0xc0, 0x06, 0x00, 0x60, 0x30, 0x00,
-  0x60, 0x78, 0x00, 0xc0, 0xfc, 0x00, 0x00, 0xfe, 0x01, 0x00, 0xff, 0x01,
-  0x80, 0xff, 0x00, 0xc0, 0x7f, 0x06, 0xc0, 0x3f, 0x06, 0x80, 0x1f, 0x0c,
-  0x80, 0x4f, 0x06, 0x19, 0xc6, 0x03, 0x1b, 0x80, 0x01, 0x73, 0x00, 0x00,
-  0x66, 0x00, 0x00, 0x0e, 0x00, 0x00, 0x3c, 0x00, 0x00, 0x70, 0x00, 0x00
-};
-
 //wave10px logo
 static const unsigned char u8g2_logo_wave[] U8X8_PROGMEM = {
   0xE0, 0x03, 0x18, 0x00, 0xC4, 0x01, 0x32, 0x00, 0x8A, 0x01, 0x69, 0x00,
@@ -71,6 +62,16 @@ const long interval = 5000; //Intervalo para leitura temp e ar
 MQ135 mq135_sensor = MQ135(pinoMQ135);
 
 void setup() {
+  Serial.begin(115200);
+  Serial.printf("\n\n**************************\n");
+  Serial.printf("IoT Bike by Estúdio Hacker\n");
+  delay(500);
+  
+  Serial.printf("Iniciando GPS...\n");
+  ss.begin(GPSBaud);
+  delay(500);
+
+  Serial.printf("Iniciando Display...\n");
   // OLED GND
   pinMode(D8, OUTPUT);
   digitalWrite(D8, LOW);
@@ -79,9 +80,8 @@ void setup() {
   pinMode(D7, OUTPUT);
   digitalWrite(D7, HIGH);
   
-  Serial.begin(9600);
-  ss.begin(GPSBaud);
   u8g2.begin();
+  delay(500);
 
   u8g2.firstPage();
   do {
@@ -152,22 +152,24 @@ void loop() {
 }
 
 void print_page1() {
-  u8g2.drawXBMP(0, 0, 20, 20, u8g_logo_sat);
-  u8g2.setFont( u8g2_font_crox1cb_tf);
-  //u8g2.setFont(u8g2_font_helvB12_tf);
-  //u8g2.setFont(u8g2_font_timB12_tf);
-  u8g2.setCursor(45, 20);
+  u8g2.setFont(u8g2_font_crox1cb_tf);
+  u8g2.setCursor(25, 10);
   u8g2.print("BIKE IoT");
-  //u8g2.setFont(u8g2_font_7x13B_tf);
-  u8g2.setFont(u8g2_font_nine_by_five_nbp_tf);
-  u8g2.setCursor(25, 35);
-  u8g2.print("by  Estudio Hacker");
+  u8g2.drawLine(0, 12, 128, 12);
+
+//  u8g2.setFont(u8g2_font_nine_by_five_nbp_tf);
+//  u8g2.setFont(u8g2_font_inb16_mf);
+  u8g2.setFont(u8g2_font_crox1cb_tf);  
+  u8g2.setCursor(0, 36);
+  u8g2.print("Estudio Hacker");
+  
   u8g2.setFont(u8g2_font_nine_by_five_nbp_tf);
   u8g2.setCursor(0, 60);
   u8g2.print("Carregando");
+  
   u8g2.setFont(u8g2_font_glasstown_nbp_tf);
-  u8g2.setCursor(40, 60);
-  u8g2.print(" . . . . . ");
+  u8g2.setCursor(60, 60);
+  u8g2.print(" . . . ");
 }
 
 void print_Clock() {
@@ -177,8 +179,8 @@ void print_Clock() {
   u8g2.drawXBMP(118, 54, 10, 10, u8g2_logo_wave);
 
   u8g2.setFont(u8g2_font_crox1cb_tf);
-  u8g2.setCursor(20, 10);
-  u8g2.print("GPS RELOGIO");
+  u8g2.setCursor(35, 10);
+  u8g2.print("RELOGIO");
   u8g2.drawLine(0, 12, 128, 12);
 
   u8g2.setFont(u8g2_font_t0_22b_tn);
@@ -193,7 +195,7 @@ void print_Clock() {
 
 void print_speed() {
   u8g2.setFont(u8g2_font_crox1cb_tf);
-  u8g2.setCursor(16, 10);
+  u8g2.setCursor(18, 10);
   u8g2.print("VELOCIDADE");
   u8g2.drawLine(0, 12, 128, 12);
 
@@ -223,12 +225,12 @@ void print_location() {
   u8g2.drawLine(0, 12, 128, 12);
 
   u8g2.setFont(u8g2_font_nine_by_five_nbp_tf);
-  u8g2.setCursor(5, 28);
+  u8g2.setCursor(0, 28);
   u8g2.print("Long: ");
   u8g2.setCursor(40, 28);
   u8g2.print( Long, 6);
 
-  u8g2.setCursor(5, 43);
+  u8g2.setCursor(0, 43);
   u8g2.print("Lat: ");
   u8g2.setCursor(40, 43);
   u8g2.print( Lat, 6);
@@ -236,7 +238,7 @@ void print_location() {
   u8g2.setCursor(0, 64);
   u8g2.print("Alt: ");
   u8g2.setCursor(20, 64);
-  u8g2.print( Alt, 3);
+  u8g2.print( Alt, 2);
 
   u8g2.setFont(u8g2_font_courB08_tn);
   u8g2.setCursor(105, 64);
@@ -246,7 +248,7 @@ void print_location() {
 
 void print_temperatura() {
   u8g2.setFont(u8g2_font_crox1cb_tf);
-  u8g2.setCursor(10, 10);
+  u8g2.setCursor(14, 10);
   u8g2.print("TEMPERATURA");
   u8g2.drawLine(0, 12, 128, 12);
 
@@ -291,6 +293,7 @@ void Get_GPS() {
     if ((Home_LAT == 0.0) && (Home_LNG == 0.0)) {
       Home_LAT = gps.location.lat();
       Home_LNG = gps.location.lng();
+      Serial.printf("GPS: HOME=%3.06f,%3.06f\n", Home_LAT, Home_LNG);
     }
     
     Lat = gps.location.lat();
@@ -401,6 +404,11 @@ static void printTime(TinyGPSTime &t) {
 }
 
 void print_Trip() {
+  u8g2.setFont(u8g2_font_crox1cb_tf);
+  u8g2.setCursor(25, 10);
+  u8g2.print("PERCURSO");
+  u8g2.drawLine(0, 12, 128, 12);
+
   unsigned long distanceKm =
     (unsigned long)TinyGPSPlus::distanceBetween(
       gps.location.lat(),
@@ -408,11 +416,11 @@ void print_Trip() {
       Home_LAT,
       Home_LNG ) / 1000.0;
   u8g2.setFont(u8g2_font_nine_by_five_nbp_tf);
-  u8g2.setCursor(0, 20);
+  u8g2.setCursor(0, 28);
   u8g2.print("Distancia: ");
-  u8g2.setCursor(60, 20);
+  u8g2.setCursor(60, 28);
   u8g2.print(distanceKm);
-  u8g2.setCursor(90, 20);
+  u8g2.setCursor(90, 28);
   u8g2.print("Km");
 
   double courseTo =
@@ -420,18 +428,18 @@ void print_Trip() {
       gps.location.lat(),
       gps.location.lng(),
       Home_LAT,
-      Home_LNG );
+      Home_LNG ) / 1000.0;
 
-  u8g2.setCursor(0, 30);
+  u8g2.setCursor(0, 43);
   u8g2.print("Curso: ");
-  u8g2.setCursor(60, 30);
+  u8g2.setCursor(60, 43);
   u8g2.print(courseTo);
-  u8g2.setCursor(90, 30);
+  u8g2.setCursor(90, 43);
   u8g2.print("Km");
 
   String cardinalTo = TinyGPSPlus::cardinal(courseTo);
-  u8g2.setCursor(0, 40);
+  u8g2.setCursor(0, 64);
   u8g2.print("Direcao: ");
-  u8g2.setCursor(60, 40);
+  u8g2.setCursor(60, 64);
   u8g2.print(cardinalTo);
 }
